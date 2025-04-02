fc604f2737e0b8e1b376b0a7dd4dd4f5501dafe4
Ava <ava.famo@gmail.com>
2025-04-02 19:41:45 +0330

    my-list

---
+++ myList.py
@@ -0,0 +1,36 @@
+
+myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+selectedNumber = myNumbers[1]
+
+# some_list[start:end:step]
+
+# selectedNumbersFromList = myNumbers[-5:]
+
+# print(selectedNumbersFromList)
+
+# copyOfNumbers = myNumbers[::2]
+
+# print(myNumbers)
+# print(copyOfNumbers)
+
+# print(myNumbers == copyOfNumbers)
+# print(myNumbers is copyOfNumbers)
+
+colors = ["red", "green", "blue", "yellow", "orange"]
+
+
+copyOfColors = colors[::-1]  # ["orange","yellow","blue","green","red"]
+
+colors.reverse()
+
+
+print(colors)
+print(copyOfColors)
+
+
+print("-----------------------------")
+
+myName = "Ava Mohseni"
+
+print(myName[:8])
